23b420ff88aa18f7a80cb2814a457d2d00c6fe04
jdlom <jdaniel.lomenede@gmail.com>
2021-10-09 17:22:09 +0200

    Initialisation du script

--- init_scripts/01_oh_my_zh.sh
+++ init_scripts/01_oh_my_zh.sh
@@ -1,9 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# install zh
+# install zsh
 sudo apt-get update
 sudo apt install -y zsh
 
-# install oh my zh
- sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /bin/zsh
+# run as rstudio
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && \
+runuser -l rstudio -c ' git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh && \
+  cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc &&\
+  chsh -s $(which zsh)'
+
+echo \
+    "
+    setHook('rstudio.sessionInit', function(newSession) {
+        if (newSession && identical(getwd(), path.expand('~')))
+        {
+            message('Zsh')
+            rstudioapi::writeRStudioPreference('posix_terminal_shell', 'zsh')
+            }
+            }, action = 'append')
+            " >> /home/rstudio/.Rprofile
