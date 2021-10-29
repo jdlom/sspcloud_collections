dbd6e365153fc1c7f6fa2380bc1cdcf639440c73
jdlom <jdaniel.lomenede@gmail.com>
2021-10-29 14:26:52 +0000

    gh auth login

--- init_scripts/99_fork_parcours_r.sh
+++ init_scripts/99_fork_parcours_r.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # fork repo from MTES-MCT
-runuser -l $THE_USER -c "gh repo list MTES-MCT -L 3000 |
+runuser -l $THE_USER -c "gh auth login && gh repo list MTES-MCT -L 3000 |
     grep -e parcours_r -e savoirfr |
     awk '{print $1}' |
     xargs -n1 -I _repository gh repo fork _repository --clone --remote"
 
 # sync already forked repo
-REPO=$(runuser -l $THE_USER -c "gh repo list -L 3000 |
+REPO=$(runuser -l $THE_USER -c "gh auth login && gh repo list -L 3000 |
     grep -e parcours_r -e savoirfr |
     awk '{print $1}'")
 
 # main or main branch
-runuser -l $THE_USER -c "echo -n $REPO | xargs -t -d ' ' -I _repo gh repo sync _repo"
+runuser -l $THE_USER -c "gh auth login && echo -n $REPO | xargs -t -d ' ' -I _repo gh repo sync _repo"
 # dev branch
-runuser -l $THE_USER -c "echo -n $REPO | xargs -t -d ' ' -I _repo gh repo sync --branch dev _repo"
+runuser -l $THE_USER -c "gh auth login && echo -n $REPO | xargs -t -d ' ' -I _repo gh repo sync --branch dev _repo"
