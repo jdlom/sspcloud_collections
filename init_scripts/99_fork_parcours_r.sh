e054ca6fb5224d1b6a780abd3ff671a918810093
jdlom <jdaniel.lomenede@gmail.com>
2021-10-29 14:54:08 +0000

    fix miss spealing savoirfR

--- init_scripts/99_fork_parcours_r.sh
+++ init_scripts/99_fork_parcours_r.sh
@@ -8,13 +8,13 @@ gh auth login --with-token $GH_TOKEN
 
 # fork repo from MTES-MCT
 gh repo list MTES-MCT -L 3000 |
-    grep -e parcours_r -e savoirfr |
+    grep -e parcours_r -e savoirfR |
     awk '{print $1}' |
     xargs -n1 -I _repository gh repo fork _repository --clone --remote
 
 # sync already forked repo
 REPO=`gh repo list -L 3000 |
-    grep -e parcours_r -e savoirfr |
+    grep -e parcours_r -e savoirfR |
     awk '{print $1}'`
 
 # main or main branch
